6cadb8e8ce4485e4ab0d73e5168bec6339bd434b
Meetpaneliya <meetpaneliya032@gmail.com>
2025-04-06 15:42:50 +0530

    .

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sendEmail = async ({ email, subject, resetpwdlink }) => {
+const sendEmail = async ({ email, subject, message }) => {
   try {
-  
+    console.log("Sending email to:", email);
+
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
@@ -19,37 +20,9 @@ const sendEmail = async ({ email, subject, resetpwdlink }) => {
     const mailOptions = {
       from: process.env.EMAIL_FROM,
       to: email,
-      subject: subject || 'StaySafe - Password Reset Request',
-      text: `
-    Hello,
-    
-    We received a request to reset your password. If you made this request, you can reset your password by clicking the link below:
-    
-    ${resetpwdlink}
-    
-    If you did not request a password reset, please ignore this email.
-    
-    Thanks,
-    The Team
-      `,
-      html: `
-        <div style="font-family: Arial, sans-serif; font-size: 16px; color: #333;">
-          <p>Hello,</p>
-          <p>We received a request to reset your password. If you made this request, you can reset your password by clicking the button below:</p>
-          <p>
-            <a href="${resetpwdlink}" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px;">
-              Reset Password
-            </a>
-          </p>
-          <p>If the button doesn't work, copy and paste the following link into your browser:</p>
-          <p><a href="${resetpwdlink}">${resetpwdlink}</a></p>
-          <br/>
-          <p>If you didn’t request this, you can safely ignore this email.</p>
-          <p>Thanks,<br/>The Team</p>
-        </div>
-      `,
+      subject: subject,
+      text: message,
     };
-
 
     const info = await transporter.sendMail(mailOptions);
     console.log("Email sent successfully:", info.response);
